aaa12dfb1a3a06b398f191500d812020d6de8bc7
justdheja <justdheja@gmail.com>
2020-05-17 14:33:36 +0700

    public

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase'
 
 const app = initializeApp({
-  apiKey: "AIzaSyDcTR29EE2_bZLBw7gcwz4w94wGsy7iyn4",
-  authDomain: "semnas-tc.firebaseapp.com",
-  databaseURL: "https://semnas-tc.firebaseio.com",
-  projectId: "semnas-tc",
-  storageBucket: "semnas-tc.appspot.com",
-  messagingSenderId: "219633004098",
-  appId: "1:219633004098:web:603cfa5cd55b2f3c01c7db",
-  measurementId: "G-2BC2G9L7BN"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 })
 
 export const db = app.firestore();
